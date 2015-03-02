dd6c14407fde7625634d7df4f632b92d0550a388
emforce <emforce@live.com>
2015-03-02 16:15:23 +0000

    added more graphs to make it look nicer, no added functionality as of yet.

--- chart.js
+++ chart.js
@@ -33,3 +33,36 @@ setTimeout(function () {
     });
 }, 2500);
 
+
+
+var chart2 = c3.generate({
+    bindto: '#chart2',
+    data: {
+      columns: [
+        ['data1', 30, 200, 100, 400, 150, 250],
+        ['data2', 50, 20, 10, 40, 15, 25]
+      ],
+      axes: {
+        data2: 'y2' // ADD
+      }
+    },
+    axis: {
+      y2: {
+        show: true // ADD
+      }
+    }
+});
+
+var chart3 = c3.generate({
+    bindto: '#chart3',
+    data: {
+        columns: [
+            ['data1', 300, 350, 300, 0, 0, 0],
+            ['data2', 130, 100, 140, 200, 150, 50]
+        ],
+        types: {
+            data1: 'area',
+            data2: 'area-spline'
+        }
+    }
+});
