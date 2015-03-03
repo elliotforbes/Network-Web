64348a7e1ccfab9e7a8d3e65231e99fe88fb8cd8
emforce <emforce@live.com>
2015-03-03 19:32:33 +0000

    shite

--- dbConn.php
+++ dbConn.php
@@ -1,7 +1,7 @@
 <?php
 //$db = new SQLite3('system/modules/test.db');
 
-$dbh = new PDO('sqlite:/system/test.db') or die("cannot open db");
+$dbh = new PDO('sqlite:/system/test.db') or die("cannot open db" . mysql_error());
 
 $query = 'SELECT * FROM connected;';
 
