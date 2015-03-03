f6d0d21ca50a2ce59ad0d85ec1c9d2d56c1d3706
emforce <emforce@live.com>
2015-03-03 18:44:53 +0000

    merged systems added sqlEngine, realtime updates

---
+++ dbConn.php
@@ -0,0 +1,18 @@
+<?php
+$db = new SQLite3('system/modules/test.db');
+
+$dbh = new PDO('sqlite:system/modules/test.db') or die("cannot open db");
+
+$query = 'SELECT * FROM connected;';
+
+
+
+foreach($dbh->query($query) as $row){
+    echo '[';
+    echo ' { "a":"' . $row[0] .'",';
+    echo '"b":"' . $row[1] .'",';
+    echo '"c":"' .$row[2] .'" } ';
+    echo ']';
+}
+
+?>
