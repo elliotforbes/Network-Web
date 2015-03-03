183a5bcc25647743e98328105ba8e0763e03e990
emforce <emforce@live.com>
2015-03-03 20:26:02 +0000

    WORKING LOCAL VERSION

--- dbConn.php
+++ dbConn.php
@@ -1,18 +1,23 @@
 <?php
-$db = new SQLite3('system/modules/test.db');
+//$db = new SQLite3('system/modules/test.db');
 
-$dbh = new PDO('sqlite:system/modules/test.db') or die("cannot open db");
-
+try{
+$dbh = new PDO('sqlite:system/test.db') or die("cannot open db");
+} catch(Exception $e) {echo $e->getMessage(); }
 $query = 'SELECT * FROM connected;';
 
+$results = $dbh->query($query);
 
-
+$outp = '[';
 foreach($dbh->query($query) as $row){
-    echo '[';
-    echo ' { "a":"' . $row[0] .'",';
-    echo '"b":"' . $row[1] .'",';
-    echo '"c":"' .$row[2] .'" } ';
-    echo ']';
+    if ($outp != "[") {$outp .= ",";}
+    $outp .= ' { "ID":"' . $row[0] .'",';
+    $outp .= '"IP":"' . $row[1] .'",';
+    $outp .= '"CONNECTED":"' . $row[2] .'",';
+    $outp .= '"LEASE_TIME":"' .$row[3] .'" } ';
+    
 }
+$outp .= ']';
 
+echo($outp);
 ?>
