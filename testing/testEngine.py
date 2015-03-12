453ccc7f422b635708f61eabe65b1876bb8b1c48
emforce <emforce@live.com>
2015-03-12 13:11:42 +0000

    updated testEngine

--- testing/testEngine.py
+++ testing/testEngine.py
@@ -2,5 +2,9 @@ from scapy.all import *
 
 def pkt_callback(pkt):
     pkt.show() # debug statement
+    
+sniffList = []
 
-sniff(iface="eth0", prn=pkt_callback, filter="ip", store=0)
+sniffList = sniff(iface="eth0", prn=pkt_callback, filter="ip", store=0, count=10)
+
+print(sniffList[4])
