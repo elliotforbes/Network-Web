f6722775a31346cb80af23953783ba127d35ebfb
emforce <emforce@live.com>
2015-03-22 16:43:20 +0000

    alertengine

--- system/modules/alertEngine.py
+++ system/modules/alertEngine.py
@@ -7,37 +7,38 @@ AUTH_TOKEN = pwords.returnAuth()
  
 client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
 
+class alertEngine():
 
-def sendAlert():
-    client.messages.create(
-        to="+447815497765", 
-        from_="+441499377047", 
-        body="Network Alert - The Network lost it's connectivity, it's back now but further action may be required.",  
-    )
-    print("Text Alert Successfully sent")
-    
+    def sendAlert():
+        client.messages.create(
+            to="+447815497765", 
+            from_="+441499377047", 
+            body="Network Alert - The Network lost it's connectivity, it's back now but further action may be required.",  
+        )
+        print("Text Alert Successfully sent")
 
-def sendThroughputAlert():
-    client.messages.create(
-        to="+447815497765",
-        from_="+441499377047",
-        body="Network Alert - Throughput rates are unusually high, please check your network's settings"
-    )
-    print("Text Alert Successfully sent")
-    
-    
-def sentDownloadAlert():
-    client.messages.create(
-        to="+447815497765",
-        from_="+441499377047",
-        body="Network Alert - Download Rates are are unusually low, please check your network's settings"
-    )
-    print("Text Alert Successfully sent")
-    
-def senUploadAlert():
-    client.messages.create(
-        to="+447815497765",
-        from_="+441499377047",
-        body="Network Alert - Upload Rates are are unusually low, please check your network's settings"
-    )
-    print("Text Alert Successfully sent")
+
+    def sendThroughputAlert():
+        client.messages.create(
+            to="+447815497765",
+            from_="+441499377047",
+            body="Network Alert - Throughput rates are unusually high, please check your network's settings"
+        )
+        print("Text Alert Successfully sent")
+
+
+    def sentDownloadAlert():
+        client.messages.create(
+            to="+447815497765",
+            from_="+441499377047",
+            body="Network Alert - Download Rates are are unusually low, please check your network's settings"
+        )
+        print("Text Alert Successfully sent")
+
+    def senUploadAlert():
+        client.messages.create(
+            to="+447815497765",
+            from_="+441499377047",
+            body="Network Alert - Upload Rates are are unusually low, please check your network's settings"
+        )
+        print("Text Alert Successfully sent")
