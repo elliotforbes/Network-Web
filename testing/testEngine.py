7bae898e4c4416a298c5bb3cf5b9108515c3c56e
emforce <emforce@live.com>
2015-04-05 00:29:13 +0100

    test engine

--- testing/testEngine.py
+++ testing/testEngine.py
@@ -1,12 +1,3 @@
 from scapy.all import *
 
-sniffList = []
-
-def pkt_callback(pkt):
-    pkt.show() # debug statement
-    print(str(pkt.getLayer(ICMP)))
-    sniffList.append(pkt)
-
-sniffList = sniff(iface="eth0", prn=pkt_callback, filter="ip", store=0, count=10)
-
-print(sniffList[4])
+tcp_syn = IP(src="192.168.1.122", dst="192.168.1.104")/TCP(dport=9000, flags="S", seq=10000)
